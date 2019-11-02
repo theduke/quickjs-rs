0653b390aa98dd4c9bdd35184372f8e240d300bc
Christoph Herzog <chris@theduke.at>
2019-11-02 23:42:12 +0100

    chore: Add _NonExhaustive variant to Value
    BREAKING CHANGE : the new variant ensures that we can add new value types
    without a semver break in the future.

--- src/bindings.rs
+++ src/bindings.rs
@@ -279,6 +279,7 @@ fn serialize_value(context: *mut q::JSContext, value: JsValue) -> Result<q::JSVa
                 js_bigint
             }
         },
+        JsValue::__NonExhaustive => unreachable!(),
     };
     Ok(v)
 }
