cee7cd5c9c17a72f3a71b83f07857045b372f0ef
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-07-30 16:45:56 +0200

    feat: start for serializer

---
+++ serde/src/errors.rs
@@ -0,0 +1,92 @@
+use std::ffi::CStr;
+use std::str::Utf8Error;
+
+use libquickjs_sys::{
+    JSContext, JSValue, JS_FreeCString, JS_FreeValue, JS_GetException, JS_IsException, JS_IsNull,
+    JS_IsString, JS_ToCStringLen2, JS_ToString,
+};
+use thiserror::Error;
+
+#[derive(Debug, Clone, Copy, Error)]
+pub enum Internal {
+    #[error("Unexpected null pointer")]
+    UnexpectedNullPointer,
+    #[error("Unexpected null value")]
+    UnexpectedNullValue,
+    #[error("Expected string")]
+    ExpectedString,
+    #[error("Invalid UTF-8")]
+    InvalidUtf8(#[from] Utf8Error),
+}
+
+unsafe fn get_string(context: &mut JSContext, value: JSValue) -> Result<String, Internal> {
+    if !JS_IsString(value) {
+        return Err(Internal::ExpectedString);
+    }
+
+    // convert to a rust string
+    let ptr = JS_ToCStringLen2(context, std::ptr::null_mut(), value, 0);
+
+    if ptr.is_null() {
+        return Err(Internal::UnexpectedNullPointer);
+    }
+
+    let c_str = CStr::from_ptr(ptr);
+
+    let string = c_str.to_str()?.to_string();
+
+    // Free the C string
+    JS_FreeCString(context, ptr);
+
+    Ok(string)
+}
+
+unsafe fn exception_to_string(
+    context: &mut JSContext,
+    exception: JSValue,
+) -> Result<String, Internal> {
+    if JS_IsNull(exception) {
+        return Err(Internal::UnexpectedNullValue);
+    }
+
+    let exception = if JS_IsString(exception) {
+        exception
+    } else {
+        JS_ToString(context, exception)
+    };
+
+    get_string(context, exception)
+}
+
+#[derive(Debug, Clone, Error)]
+pub enum SerializationError {
+    #[error("Out of memory")]
+    OutOfMemory,
+    #[error("Internal error: {0}")]
+    Internal(#[from] Internal),
+    #[error("Unknown error: {0}")]
+    Unknown(String),
+}
+
+impl SerializationError {
+    pub fn from_value(context: &mut JSContext, value: JSValue) -> Result<JSValue, Self> {
+        if unsafe { JS_IsException(value) } {
+            // we're for sure an error, we just don't know which one
+            // TODO: do we need to free here?
+            unsafe { JS_FreeValue(context, value) }
+
+            // https://bellard.org/quickjs/quickjs.html#Exceptions 3.4.4
+            let exception = unsafe { JS_GetException(context) };
+
+            let value = unsafe { exception_to_string(context, exception) }?;
+
+            if value.contains("out of memory") {
+                return Err(Self::OutOfMemory);
+            }
+
+            Err(Self::Unknown(value))
+        } else {
+            Ok(value)
+        }
+    }
+}
--- serde/src/ser.rs
+++ serde/src/ser.rs
@@ -1,19 +1,22 @@
-use libquickjs_sys::JSContext;
+use libquickjs_sys::{
+    JSContext, JSValue, JS_IsException, JS_NewBigInt64, JS_NewBigUint64, JS_NewBool, JS_NewFloat64,
+    JS_NewInt32,
+};
 use serde::Serialize;
 
 pub struct Serializer<'a> {
-    context: &'a JSContext,
+    context: &'a mut JSContext,
 }
 
 impl<'a> Serializer<'a> {
-    pub fn new(context: &'a JSContext) -> Self {
+    pub fn new(context: &'a mut JSContext) -> Self {
         Self { context }
     }
 }
 
 impl<'a> serde::Serializer for Serializer<'a> {
     type Error = ();
-    type Ok = ();
+    type Ok = JSValue;
     type SerializeMap = ();
     type SerializeSeq = ();
     type SerializeStruct = ();
@@ -22,67 +25,91 @@ impl<'a> serde::Serializer for Serializer<'a> {
     type SerializeTupleStruct = ();
     type SerializeTupleVariant = ();
 
-    fn serialize_bool(self, v: bool) -> Result<Self::Ok, Self::Error> {
+    fn serialize_bool(self, value: bool) -> Result<Self::Ok, Self::Error> {
+        let value = unsafe { JS_NewBool(self.context, value) };
+
+        Ok(value)
+    }
+
+    fn serialize_i8(self, value: i8) -> Result<Self::Ok, Self::Error> {
+        self.serialize_i32(i32::from(value))
+    }
+
+    fn serialize_i16(self, value: i16) -> Result<Self::Ok, Self::Error> {
+        self.serialize_i32(i32::from(value))
+    }
+
+    fn serialize_i32(self, value: i32) -> Result<Self::Ok, Self::Error> {
+        let value = unsafe { JS_NewInt32(self.context, value) };
+
+        Ok(value)
+    }
+
+    fn serialize_i64(self, value: i64) -> Result<Self::Ok, Self::Error> {
+        // try to fit the value into a 32-bit integer, otherwise return a BigInt
+        if let Ok(value) = i32::try_from(value) {
+            return self.serialize_i32(value);
+        }
+
+        let value = unsafe { JS_NewBigInt64(self.context, value) };
+
+        Ok(value)
+    }
+
+    // For now we don't support i128 and u128, as there are no methods to create
+    // BigInts for them.
+    // In theory we could create our own function to do so, but for now that's
+    // overkill.
+
+    fn serialize_u8(self, value: u8) -> Result<Self::Ok, Self::Error> {
+        self.serialize_i32(i32::from(value))
+    }
+
+    fn serialize_u16(self, value: u16) -> Result<Self::Ok, Self::Error> {
+        self.serialize_i32(i32::from(value))
+    }
+
+    fn serialize_u32(self, value: u32) -> Result<Self::Ok, Self::Error> {
+        // we cannot use `JS_NewInt32` here, as there are values in u32 that cannot be
+        // represented in i32 (and would wrap around)
+        self.serialize_u64(u64::from(value))
+    }
+
+    fn serialize_u64(self, value: u64) -> Result<Self::Ok, Self::Error> {
+        // try to fit the value into a 32-bit integer, otherwise return a BigInt
+        // we could also call `serialize_u64` instead, but that is largely redundant.
+        if let Ok(value) = i32::try_from(value) {
+            return self.serialize_i32(value);
+        }
+
+        let value = unsafe { JS_NewBigUint64(self.context, value) };
+
+        Ok(value)
+    }
+
+    fn serialize_f32(self, value: f32) -> Result<Self::Ok, Self::Error> {
+        let value = unsafe { JS_NewFloat64(self.context, f64::from(value)) };
+
+        if JS_IsException(value) {
+            return Err(());
+        }
+
+        Ok(value)
+    }
+
+    fn serialize_f64(self, value: f64) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
-    fn serialize_i8(self, v: i8) -> Result<Self::Ok, Self::Error> {
+    fn serialize_char(self, value: char) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
-    fn serialize_i16(self, v: i16) -> Result<Self::Ok, Self::Error> {
+    fn serialize_str(self, value: &str) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
-    fn serialize_i32(self, v: i32) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
-
-    fn serialize_i64(self, v: i64) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
-
-    fn serialize_i128(self, v: i128) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
-
-    fn serialize_u8(self, v: u8) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
-
-    fn serialize_u16(self, v: u16) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
-
-    fn serialize_u32(self, v: u32) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
-
-    fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
-
-    fn serialize_u128(self, v: u128) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
-
-    fn serialize_f32(self, v: f32) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
-
-    fn serialize_f64(self, v: f64) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
-
-    fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
-
-    fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
-
-    fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok, Self::Error> {
+    fn serialize_bytes(self, value: &[u8]) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
